72e60faaea9e2b4f92794804fe32d44d157b2158
Tugsbayasgalan Manlaibaatar <tmanlaibaatar@fb.com>
2024-10-16 16:33:45 -0700

    Fix assigning tensor with requires_grad as constant in export
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/137997
    
    When we insert cojstants into unlifted graph, we need to detach them if they require grad.
    ghstack-source-id: b21846840877f939537c3b682a3235288c9209d7
    
    Differential Revision: [D64406859](https://our.internmc.facebook.com/intern/diff/D64406859/)

--- test/export/test_export.py
+++ test/export/test_export.py
@@ -7577,6 +7577,18 @@ def forward(self, x, y):
         self.assertTrue(torch.allclose(a, torch.ones(4, 4)))
         self.assertTrue(torch.allclose(b, torch.ones(4, 4)))
 
+    def test_constant_requires_grad_const(self):
+        class M(torch.nn.Module):
+            def __init__(self):
+                super().__init__()
+                self.foo = torch.randn(2, 2, requires_grad=True)
+
+            def forward(self, x):
+                return x.cos() + self.foo.sum()
+
+        gm = export(M(), (torch.ones(2, 2),)).module()
+        self.assertFalse(gm.foo.requires_grad)
+
     def test_constant_aliasing(self):
         class M1(torch.nn.Module):
             def __init__(self, m2, foo):
@@ -7590,7 +7602,7 @@ def forward(self, x, y):
         class M2(torch.nn.Module):
             def __init__(self) -> None:
                 super().__init__()
-                self.foo = torch.ones(3, 3)
+                self.foo = torch.ones(3, 3, requires_grad=True)
 
             def forward(self, x):
                 return x + self.foo
