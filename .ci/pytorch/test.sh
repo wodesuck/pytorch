560f114f953ae2f0e0e9f163a9ab892bc23e57a7
chuanqiw <chuanqi.wang@intel.com>
2024-11-05 19:35:25 +0100

    [CI] Upgrade XPU support packages version to 2025.0 for linux CI

--- .ci/pytorch/test.sh
+++ .ci/pytorch/test.sh
@@ -172,6 +172,10 @@ if [[ "$BUILD_ENVIRONMENT" == *xpu* ]]; then
   # refer to https://www.intel.com/content/www/us/en/developer/articles/tool/pytorch-prerequisites-for-intel-gpu/2-5.html
   # shellcheck disable=SC1091
   source /opt/intel/oneapi/compiler/latest/env/vars.sh
+  if [ -f /opt/intel/oneapi/umf/latest/env/vars.sh ]; then
+    # shellcheck disable=SC1091
+    source /opt/intel/oneapi/umf/latest/env/vars.sh
+  fi
   # Check XPU status before testing
   xpu-smi discovery
 fi
